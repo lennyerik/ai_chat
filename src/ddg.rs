2f6be97df112d8f5cdfd057b152d7180d92dec64
lennyerik <49204493+lennyerik@users.noreply.github.com>
2024-12-10 21:28:43 +0100

    crate instead of super

--- src/ddg.rs
+++ src/ddg.rs
@@ -5,7 +5,7 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
-use super::llm;
+use crate::llm;
 
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
 
